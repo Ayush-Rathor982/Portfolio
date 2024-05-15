7376596aa22c1c300e756b585bd5036757a44d7e
Ayush-Rathor982 <146756530+Ayush-Rathor982@users.noreply.github.com>
2024-05-15 15:50:45 +0530

    Update Intro.jsx

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,7 +2,7 @@ export default function Intro() {
     return (
         <div className="cont">
             <div className="intro text-center d-flex flex-column justify-content-center">
-                <h1>Hello! denizens of digital frontier.</h1>
+                <h1>Hello! denizen of digital frontier.</h1>
                 <h2>I'm&nbsp;&nbsp;<span>Ayush Rathor</span>.</h2>
                 <h2>Very passionate about programming.</h2>
             </div>
